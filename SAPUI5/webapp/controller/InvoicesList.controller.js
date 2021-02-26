c29459e093896e1223a21f849dd42b26d7adba11
eduardoruizmenendez <eduardoruizmenendez@gmail.com>
2021-02-26 13:01:57 +0000

    backup

---
+++ SAPUI5/webapp/controller/InvoicesList.controller.js
@@ -0,0 +1,24 @@
+//@ts-check
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+     */
+    function (Controller, JSONModel) {
+        return Controller.extend("logaligroup.SAPUI5.controller.InvoicesList",
+            {
+                onInit: function(){
+                    var oViewModel = new JSONModel({
+                        usd: "USD",
+                        eur: "EUR"
+                    });
+
+                    this.getView().setModel(oViewModel, "currency");
+
+                }
+            });
+    });
