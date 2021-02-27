f3c11aaf91d8f789bd137c8e83f8a7cc02f04930
eduardoruizmenendez <eduardoruizmenendez@gmail.com>
2021-02-27 19:00:50 +0000

    Backup

--- SAPUI5/webapp/controller/InvoicesList.controller.js
+++ SAPUI5/webapp/controller/InvoicesList.controller.js
@@ -1,16 +1,23 @@
 //@ts-check
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "../model/invoicesFormatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+     * @param {typeof sap.ui.model.Filter} Filter
+     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, invoicesFormatter, Filter, FilterOperator) {
         return Controller.extend("logaligroup.SAPUI5.controller.InvoicesList",
             {
+                invoicesFormatter: invoicesFormatter,
+
                 onInit: function(){
                     var oViewModel = new JSONModel({
                         usd: "USD",
@@ -19,6 +26,19 @@ sap.ui.define([
 
                     this.getView().setModel(oViewModel, "currency");
 
+                },
+
+                onFilterinvoices: function(oEvent){
+                    const arrayFilter = [];
+                    const sQuery = oEvent.getParameter("query");
+
+                    if(sQuery){
+                        arrayFilter.push( new Filter("ProductName", FilterOperator.Contains, sQuery));
+                    };
+
+                    const oList = this.getView().byId("invoiceList");
+                    const oBinding = oList.getBinding("items");
+                    oBinding.filter(arrayFilter);
                 }
             });
     });
---
+++ SAPUI5/webapp/model/invoicesFormatter.js
@@ -0,0 +1,17 @@
+//@ts-check
+sap.ui.define([
+
+],
+    function () {
+        return {
+            invoiceStatus : function(sStatus){
+                const resourceBundle = this.getView().getModel("i18n").getResourceBundle();
+                switch (sStatus) {
+                    case 'A': return resourceBundle.getText("InvoiceStatusA");
+                    case 'B': return resourceBundle.getText("InvoiceStatusB");
+                    case 'C': return resourceBundle.getText("InvoiceStatusC");
+                    default: return sStatus;
+                }
+            }
+        }
+    });
